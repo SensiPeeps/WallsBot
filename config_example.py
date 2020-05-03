12256a970d395e67e06849db343e23322b2efc1e
starry69 <starry369126@outlook.com>
2020-05-03 10:48:35 +0530

    Add long polling method along with webhooks, & add error callback handler
    Signed-off-by: starry69 <starry369126@outlook.com>

---
+++ config_example.py
@@ -0,0 +1,3 @@
+TOKEN = None # Bot token from botfather
+PIX_API = None # Pixaby API KEY
+WEBHOOK = False # Dont set True in long polling! only for ENV method
--- starrybot.py
+++ starrybot.py
@@ -12,39 +12,96 @@
 
 
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, run_async
 import logging
 import os
 
+from telegram import (Message,
+                      Chat,
+                      Update,
+                      Bot,
+                      User,
+                      ChatAction,
+                      ParseMode)
+
+from telegram.error import BadRequest
+from functools import wraps
 
 # enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.DEBUG)
+    level=logging.INFO)
 
 LOGGER = logging.getLogger(__name__)
 
-# Currently only webhook mode is supported.
+ENV = bool(os.environ.get('ENV', False))
 
-TOKEN = os.environ.get('TOKEN')
-URL = os.environ.get('URL')
-PORT = int(os.environ.get('PORT'))
-PIX_API = os.environ.get('PIX_API', None)
+if ENV:
+       TOKEN = os.environ.get('TOKEN')
+       URL = os.environ.get('URL')
+       PORT = int(os.environ.get('PORT'))
+       WEBHOOK = bool(os.environ.get('WEBHOOK', False))
+       PIX_API = os.environ.get('PIX_API', None)
+else:
+      from config import TOKEN, PIX_API, WEBHOOK
+
 
 updater = Updater(TOKEN, use_context=True)
 
 dispatcher = updater.dispatcher
 
+##########################################
+
+def typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+        return func(update, context,  *args, **kwargs)
+
+    return command_func
+
+START_MSG = f"""Heya my name is <b>{dispatcher.bot.first_name}</b>.
+            \nI'm here to give you some cool high definition wallpapers.
+            \nClick: /help to get list of commands!"""
+
+HELP_MSG = """Help the helper"""
+
+@run_async
+@typing_action
+def help(update, context):
+    update.effective_message.reply_text(HELP_MSG, parse_mode=ParseMode.HTML)
+
+@run_async
+@typing_action
 def start(update, context):
-    update.effective_message.reply_text("Hello!")
+    update.effective_message.reply_text(START_MSG, parse_mode=ParseMode.HTML)
+
+# Log Errors caused by Updates
+
+def error(update, context):
+    LOGGER.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+help_handler = CommandHandler('help', help)
 
-LOGGER.info("Starting WALLSBOT | Using webhook...")
-updater.start_webhook(listen="0.0.0.0",
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_error_handler(error)
+
+##########################################
+if WEBHOOK:
+          LOGGER.info("Starting WallpaperRobot | Using webhook...")
+          updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
 
-updater.bot.set_webhook(url=URL + TOKEN)
+          updater.bot.set_webhook(url=URL + TOKEN)
+
+else:
+     LOGGER.info("Starting WAallpaperRobot | Using long polling...")
+     updater.start_polling(timeout=15, read_latency=4)
+
 updater.idle()
